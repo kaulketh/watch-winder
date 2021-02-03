f68a69fcb1576d700bf114c4a27f9c065c49ce1e
Thomas <kaulketh@gmail.com>
2021-02-03 22:14:58 +0100

    Update

--- ww.py
+++ ww.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # -----------------------------------------------------------
-# created 02.02.2021
+# created 03.02.2021
 # Thomas Kaulke, kaulketh@gmail.com
 # https://github.com/kaulketh
 # -----------------------------------------------------------
 import random
-from time import sleep, strftime, localtime
+from datetime import datetime
+from time import sleep
 
 from SM_28BYJ48 import SM28BYJ48
 
@@ -18,19 +19,19 @@ MOTOR = SM28BYJ48(6, 13, 19, 26)
 ANGLES = (45, 90, 180, 270, 360)
 
 
-def angle_mode_1(turns=1, sleep_time=1.5):
+def angle_mode(turns=1, sleep_time=1.5):
     while turns > 0:
         turns -= 1
         for _ in range(len(ANGLES)):
             i = random.randint(0, len(ANGLES) - 1)
-            MOTOR.rotate(ANGLES[i] * -1)
+            MOTOR.rotate(ANGLES[i] * random_direction())
             sleep(sleep_time)
-    __wait_for_next_turn(900, 1800)
+    wait_for_next_turn(600, 1_800)
 
 
-def random_mode_1(sleep_time=1.5):
+def full_random_mode(sleep_time=1.5):
     turns = random.randint(1, 3)
-    MOTOR.logger.info(
+    MOTOR.logger.debug(
         f"Turn {turns} times clockwise/counter clockwise.")
     ccw_steps = random.randint(1_024, 4_096)
     cw_steps = random.randint(-4_096, -1_024)
@@ -39,30 +40,45 @@ def random_mode_1(sleep_time=1.5):
         sleep(sleep_time)
         MOTOR.step(ccw_steps)
         sleep(sleep_time)
-    __wait_for_next_turn()
+    wait_for_next_turn()
 
 
-def __wait_for_next_turn(range_begin=1_800, range_end=3600):
+def wait_for_next_turn(range_begin=1_800, range_end=3600):
     wait = random.randint(range_begin, range_end)
-    timestamp = strftime('%H:%M:%S', localtime())
     MOTOR.logger.info(
-        f"{timestamp} Waiting {round(wait / 60, 2)} minutes until next movement")
+        f"Waiting {round(wait / 60, 1)} minutes until next movement")
     sleep(wait)
 
 
+def current_hour():
+    return datetime.now().hour
+
+
+def random_direction():
+    d = random.randint(-1, 0)
+    return d if d != 0 else 1
+
+
 if __name__ == '__main__':
-    MOTOR.logger.info(MOTOR)
+    MOTOR.logger.debug(MOTOR)
+    count = 1
     while True:
         try:
-            # insert function here
-            random_mode_1()
-            # angle_mode_1()
-            # angle_mode_1(turns=5, sleep_time=3)
+            if 8 <= current_hour() <= 22:
+                # insert function here
+                angle_mode(sleep_time=3)
+
+                count = 1
+            else:
+                # Wait for the end of the night
+                if count > 0:
+                    MOTOR.logger.info("Night rest! ;-)")
+                    count -= 1
+                sleep(60)
         except KeyboardInterrupt:
-            MOTOR.logger.warning(f"\nInterrupted by user input")
+            MOTOR.logger.warning(f"Interrupted by user input")
             exit(1)
         except Exception as e:
             MOTOR.logger.error(f"Any error occurs: {e}")
-            exit(1)
-        finally:
             MOTOR.reset()
+            exit(1)
