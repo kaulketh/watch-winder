6092ab461aaa9ce4e6f0b3b1842c654d1a215117
Thomas K <kaulketh@gmail.com>
2021-03-08 12:21:19 +0100

    Update status led II

--- status/led.py
+++ status/led.py
@@ -13,12 +13,14 @@ import os
 
 import RPi.GPIO as GPIO
 
+from logger import LOGGER
+
 
 class StatusLed():
 
     def __init__(self, pin_red: int, pin_blue: int):
         self.__pins = (pin_red, pin_blue)  # RED, BLUE
-
+        LOGGER.debug(f"Init {self}")
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(self.__pins[0], GPIO.OUT)
@@ -26,23 +28,28 @@ class StatusLed():
 
     def red(self):
         self.off()
+        LOGGER.debug("Enable red light")
         GPIO.output(self.__pins[0], GPIO.HIGH)
 
     def blue(self):
         self.off()
+        LOGGER.debug("Enable blue light")
         GPIO.output(self.__pins[1], GPIO.HIGH)
 
     def blink_red(self):
         self.off()
+        LOGGER.debug("Enable blinking red light")
         os.system('gpio -g mode 20 out')
         os.system('gpio -g blink 20')
 
     def blink_blue(self):
         self.off()
+        LOGGER.debug("Enable blinking blue light")
         os.system('gpio -g mode 21 out')
         os.system('gpio -g blink 21')
 
     def off(self):
+        LOGGER.debug("Disable led")
         for pin in self.__pins:
             GPIO.output(pin, GPIO.LOW)
 
