14e373d8fa592a9352648763c8bf9051902d1afb
Thomas K <kaulketh@gmail.com>
2021-04-10 00:36:59 +0200

    Update watch_winder_v1.ino

--- arduino/v1/watch_winder_v1.ino
+++ arduino/v1/watch_winder_v1.ino
@@ -2,14 +2,6 @@
 // Driver ULN2003, motor reduced to 1:64
 //**************************************************************
 
-// JLed breathe demo.
-// Copyright 2017 by Jan Delgado. All rights reserved.
-// https://github.com/jandelgado/jled
-#include <jled.h>
-
-// breathe LED for 5 times, LED is connected to pin 9 (PWM capable) gpio
-auto led = JLed(5).Breathe(2000).Repeat(5).DelayAfter(2000);
-
 #include <Stepper.h>
 // LED
 #define LED 5
